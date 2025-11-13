8d1d85e16aec693894630088ac182c9da14da7df
victor-roliveira <victorramos123@outlook.com>
2025-11-13 13:15:07 -0300

    fix: adjust router url

--- api/server.js
+++ api/server.js
@@ -18,27 +18,27 @@ app.get('/', (req, res) => {
 })
 
 // rotas das semanas
-app.get('/weeks', async (req, res) => {
+app.get('/api/weeks', async (req, res) => {
     const weeks = await prisma.week.findMany({ include: { cards: true } })
     res.json(weeks)
 })
-app.post('/weeks', async (req, res) => {
+app.post('/api/weeks', async (req, res) => {
     const { name, dateRange } = req.body
     const newWeek = await prisma.week.create({ data: { name, dateRange } })
     res.json(newWeek)
 })
 
 // rotas dos cartões
-app.get('/cards', async (req, res) => {
+app.get('/api/cards', async (req, res) => {
     const cards = await prisma.card.findMany({ include: { disciplines: true, week: true } })
     res.json(cards)
 })
-app.post('/cards', async (req, res) => {
+app.post('/api/cards', async (req, res) => {
     const { title, orderService, weekId } = req.body
     const newCard = await prisma.card.create({ data: { title, orderService, weekId } })
     res.json(newCard)
 })
-app.put('/cards/:id', async (req, res) => {
+app.put('/api/cards/:id', async (req, res) => {
     const { id } = req.params
     const data = req.body
     const updated = await prisma.card.update({
@@ -48,19 +48,19 @@ app.put('/cards/:id', async (req, res) => {
     res.json(updated)
 })
 
-app.delete('/cards/:id', async (req, res) => {
+app.delete('/api/cards/:id', async (req, res) => {
     const { id } = req.params
     await prisma.card.delete({ where: { id: Number(id) } })
     res.json({ message: 'Cartão removido!' })
 })
 
 // rotas das disciplinas
-app.post('/disciplines', async (req, res) => {
+app.post('/api/disciplines', async (req, res) => {
     const { name, color, icon, cardId } = req.body
     const newDiscipline = await prisma.discipline.create({ data: { name, color, icon, cardId } })
     res.json(newDiscipline)
 })
-app.delete('/disciplines/:id', async (req, res) => {
+app.delete('/api/disciplines/:id', async (req, res) => {
     const { id } = req.params
     await prisma.discipline.delete({ where: { id: Number(id) } })
     res.json({ message: 'Disciplina removida' })
